f1c0195cc4dcc650f7d21afa32c9cae8bf946a01
E969431_ibe <E969431@iberdrola.com>
2026-01-03 03:35:40 +0100

    feat: add SVG icons and video assets; configure Tailwind CSS and TypeScript
    - Added new SVG icons: next.svg and vercel.svg.
    - Included video assets: cinematic rotating shot of cookies and kneading dough.
    - Configured Tailwind CSS with a custom theme and updated globals.css.
    - Set up TypeScript configuration with strict settings and module resolution.
    - Created type definitions for SVG props in index.ts.

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+import { title } from "@/components/primitives";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-xl text-center justify-center">
+        <span className={title()}>Busca &nbsp;</span>
+        <span className={title({ color: "violet" })}>Lo Más Rico,&nbsp;</span>
+        <br />
+        <span className={title()}>Lo Más Vital.</span>
+        <div className="flex flex-col gap-4 mt-8">
+          <video
+            autoPlay
+            className="w-full rounded-md shadow-lg opacity-85"
+            loop
+            muted
+            src="/vecteezy_knead-the-dough-ingredients-for-the-dough-the-best_14348716.mov"
+          />
+          {/* <video
+            autoPlay
+            className="w-full rounded-md shadow-lg opacity-85"
+            loop
+            muted
+            src="/vecteezy_cinematic-rotating-shot-of-cookies-on-a-plate-cookies-386_1619181.mp4"
+          /> */}
+        </div>
+      </div>
+    </section>
+  );
+}
